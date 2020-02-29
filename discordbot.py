21df1fcdf5e76e32120e1b438418f8bea885df9f
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-02-29 15:05:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,37 @@
-from discord.ext import commands
+#coding:UTF-8
 import os
-import traceback
+import discord
+from discord.ext import tasks
+from datetime import datetime 
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+pytoken = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = 653100735281758208 #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+# 60秒に一回ループ
+await client.wait_until_ready()
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '15:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('3月1日(日)')  
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    if now == '04:30':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('@everyone\nクラン活動\n30分前')  
 
+    if now == '05:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('@everyone\nクラン活動')  
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('呼んだ？')
+    if now == '06:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('@everyone\nクラン活動\n終了') 
 
-
-bot.run(token)
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(pytoken)
