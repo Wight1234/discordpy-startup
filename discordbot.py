7d36818471241058f7177b800c6fcf0d05a6599d
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-03-09 13:49:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,6 +19,14 @@ async def loop():
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('3月10日(火)\n本日はクラン活動があります。')  
 
+    if now == '09:00':
+        channel = client.get_channel(CHANNEL_ID)
+
+    A_user = discord.utils.get(client.users,name="VE1L")
+
+    reply = A_user.mention + "さん\n幸福はノスタルジーに眠る杯\n30分前"
+    await channel.send(reply)
+
     if now == '11:30':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('@everyone\nクラン活動\n30分前')  
