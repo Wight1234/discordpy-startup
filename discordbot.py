6ffabebdfea3f673bef45411465e50df6816c3fb
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-03-18 00:15:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,21 +15,9 @@ async def loop():
     await client.wait_until_ready()
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '15:15':
+    if now == '15:16':
         channel = client.get_channel(CHANNEL_ID)
-        await channel.send('3月18日(水)\n本日クラン活動はありません')  
-
-    if now == '11:30':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@everyone\nクラン活動\n30分前')  
-
-    if now == '12:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@everyone\nクラン活動')  
-
-    if now == '13:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@everyone\nクラン活動\n終了') 
+        await channel.send('3月19日(木)\n本日クラン活動はありません。')  
 
 #ループ処理実行
 loop.start()
