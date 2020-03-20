0a17b7e40d04988809e84364e9d90932981b7c03
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-03-21 00:38:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def loop():
     await client.wait_until_ready()
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '15:38':
+    if now == '15:00':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('3月21日(土)\n本日21時よりクラン活動があります。')  
 
