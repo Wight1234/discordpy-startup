58c3379129784fab5ed8b519c10c55b5ad991c58
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-03-21 23:09:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,17 +15,25 @@ async def loop():
     await client.wait_until_ready()
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
+    if now == '14:56':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('問題')  
+
+    if now == '14:57':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('明日は何の日でしょうか。')  
+
+    if now == '14:58':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('正解は、')  
+
+    if now == '14:59':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('やまとの誕生日です。')  
+
     if now == '15:00':
         channel = client.get_channel(CHANNEL_ID)
-        await channel.send('3月21日(土)\n本日21時よりクラン活動があります。')  
-
-    if now == '08:30':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@everyone\nアジア ハイプナイト\n30分前') 
-
-    if now == '12:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@here\nアジア　ハイプナイト結果\nhttps://fortnitetracker.com/events?region=ASIA')  
+        await channel.send('3月22日(日)\n本日21時よりクラン活動があります。')  
 
     if now == '11:30':
         channel = client.get_channel(CHANNEL_ID)
