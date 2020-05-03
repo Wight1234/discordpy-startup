9ec93e440df96fca03ff6334fe7ea11058c09b4c
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-05-03 19:43:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ from discord.ext import tasks
 from datetime import datetime 
 
 pytoken = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID =  #チャンネルID
+CHANNEL_ID = 685060700896493578 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -15,9 +15,9 @@ async def loop():
     await client.wait_until_ready()
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '10:29':
+    if now == '00:00':
         channel = client.get_channel(CHANNEL_ID)
-        await channel.send('3月23日(月)\n本日クラン活動はありません。')  
+        await channel.send('')  
 
 #ループ処理実行
 loop.start()
