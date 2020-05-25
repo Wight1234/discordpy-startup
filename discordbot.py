277e84294f6285575f8d1b7787eb450b85a0015c
Wight1234 <61611921+Wight1234@users.noreply.github.com>
2020-05-25 12:41:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ from discord.ext import tasks
 from datetime import datetime 
 
 pytoken = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 685060700896493578 #チャンネルID
+CHANNEL_ID = 713696619177967703 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -15,9 +15,9 @@ async def loop():
     await client.wait_until_ready()
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '00:00':
+    if now == '03:42':
         channel = client.get_channel(CHANNEL_ID)
-        await channel.send('')  
+        await channel.send('@everyone カスタムキー')  
 
 #ループ処理実行
 loop.start()
